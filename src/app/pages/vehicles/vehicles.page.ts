821f39ba3f7c50d224e130434257fc8c0e683202
Sergiom8m <sergiomartinportillo8@gmail.com>
2024-10-16 15:46:53 +0200

    Vehicle state update with a modal

--- src/app/pages/vehicles/vehicles.page.ts
+++ src/app/pages/vehicles/vehicles.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { AlertController } from '@ionic/angular';
 import { Vehicle } from 'src/app/models';
 import { FirestoreService } from 'src/app/services/firestore.service';
 
@@ -11,10 +12,11 @@ import { FirestoreService } from 'src/app/services/firestore.service';
 export class VehiclesPage implements OnInit {
 
   vehicles: any[] = []; 
-  path = "Vehiculos/"
+  path = "Vehiculos/";
 
   constructor(
     private firestoreService: FirestoreService,
+    private alertController: AlertController,
     private router: Router
   ) { }
 
@@ -30,6 +32,63 @@ export class VehiclesPage implements OnInit {
 
   goToReservation(vehicleId: string) {
     this.router.navigate(['/reservation'], { queryParams: { vehicle_id: vehicleId } });
+  }
+
+  async editVehicle(vehicle: Vehicle) {
+    const alert = await this.alertController.create({
+      header: 'Editar Vehículo',
+      inputs: [
+        {
+          name: 'needsRepair',
+          type: 'checkbox',
+          label: 'Averia',
+          value: 'needsRepair', 
+          checked: vehicle.needsRepair 
+        },
+        {
+          name: 'needsFuel',
+          type: 'checkbox',
+          label: 'Combustible bajo',
+          value: 'needsFuel', 
+          checked: vehicle.needsFuel 
+        }
+      ],
+      buttons: [
+        {
+          text: 'Cancelar',
+          role: 'cancel',
+          cssClass: 'secondary',
+          handler: () => {
+            console.log('Cancelado');
+          }
+        },
+        {
+          text: 'Guardar',
+          handler: (data) => {
+            const needsRepair = data.includes('needsRepair');
+            const needsFuel = data.includes('needsFuel');
+
+            this.saveChanges(vehicle, needsRepair, needsFuel);
+          }
+        }
+      ]
+    });
+
+    await alert.present();
+  }
+
+  saveChanges(vehicle: Vehicle, needsRepair: boolean, needsFuel: boolean) {
+
+    vehicle.needsRepair = needsRepair;
+    vehicle.needsFuel = needsFuel;
+
+    this.firestoreService.updateDoc(vehicle, this.path, vehicle.licensePlate)
+      .then(() => {
+        console.log('Vehículo actualizado');
+      })
+      .catch(error => {
+        console.error('Error al actualizar vehículo:', error);
+      });
   }
 
 }
