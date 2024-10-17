04e7ded18c32d61fc523e4c2173541bcbc61f5c2
Sergiom8m <sergiomartinportillo8@gmail.com>
2024-10-17 12:23:53 +0200

    Reservation list UI done

--- src/app/pages/reservations/reservations.page.ts
+++ src/app/pages/reservations/reservations.page.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { AlertController } from '@ionic/angular';
+import { Vehicle } from 'src/app/models';
+import { FirestoreService } from 'src/app/services/firestore.service';
 
 @Component({
   selector: 'app-reservations',
@@ -9,10 +11,31 @@ import { AlertController } from '@ionic/angular';
 export class ReservationsPage {
   selectedDate: string;
   formattedDate: string;
+  vehicles: any[] = []; // Para almacenar los vehículos
+  selectedVehicle: Vehicle | null = null; // Para manejar el vehículo seleccionado
+  path_vehicles = "Vehiculos/";
 
-  constructor(private alertController: AlertController) {
+  constructor(
+    private alertController: AlertController,
+    private firestoreService: FirestoreService
+  ) {
     this.selectedDate = new Date().toISOString(); // Inicializa con la fecha actual
     this.formattedDate = this.formatDate(this.selectedDate);
+  }
+
+  ngOnInit() {
+    this.loadVehicles(); // Carga los vehículos al inicializar
+    console.log(this.vehicles);
+  }
+
+  loadVehicles() {
+    this.firestoreService.getCollection<Vehicle>(this.path_vehicles).subscribe((vehicles) => {
+      this.vehicles = vehicles; // Almacena los vehículos en la variable
+    });
+  }
+
+  toggleVehicle(vehicle: Vehicle) {
+    this.selectedVehicle = this.selectedVehicle === vehicle ? null : vehicle; // Alternar entre mostrar y ocultar detalles
   }
 
   formatDate(date: string) {
@@ -69,5 +92,10 @@ export class ReservationsPage {
     currentDate.setDate(currentDate.getDate() + 1);
     this.selectedDate = currentDate.toISOString();
     this.formattedDate = this.formatDate(this.selectedDate);
+  }
+
+  getVehicleImage(model: string): string {
+    const imageName = model.toLowerCase().replace(/\s+/g, '_'); // Convierte "Dacia Duster" a "dacia_duster"
+    return `../../../assets/${imageName}.png`; // Asumiendo que la imagen sigue el patrón [nombre].png
   }
 }
