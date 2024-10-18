8d5aa006b3a9dadbf236c202f31b19026a89b6ab
Sergiom8m <sergiomartinportillo8@gmail.com>
2024-10-18 11:33:52 +0200

    Production env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyAboEA9T1gCGEvgFZTHelJGKNpzmKbIdBU",
+    authDomain: "pruebasergioionic.firebaseapp.com",
+    projectId: "pruebasergioionic",
+    storageBucket: "pruebasergioionic.appspot.com",
+    messagingSenderId: "115778028812",
+    appId: "1:115778028812:web:29ba43f4e9aa3c47c43529",
+    measurementId: "G-1BYMJ6Z3LP"
+  }
 };
